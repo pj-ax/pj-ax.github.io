07a765843d3f6fca8f7a9770824f6b7773df53c6
pj-ax <trap2@oh.rr.com>
2015-08-31 22:20:21 -0400

    freecodecamp random quotes zipline

---
+++ fcc_random_quotes_zipline/quotes.js
@@ -0,0 +1,32 @@
+﻿function getNewQuote() {
+    $.ajax({
+        method: "GET",
+        url: "http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en",
+        dataType: "jsonp",
+        jsonp: "jsonp"
+    }).done(function (q) {
+        $('#quote').text(q.quoteText);
+        if (q.quoteAuthor) {
+            $('.quote-author').text(q.quoteAuthor);
+        } else {
+            $('.quote-author').text('Unknown');
+        }
+        twttr.widgets.createShareButton(
+            'http://www.freecodecamp.com',
+            document.getElementById('twt'),
+            {
+                text: q.quoteText + ' -' + $('.quote-author').text() + '-' + '\n',
+                hashtags: 'RandomQuotes',
+                count: 'none'
+            }
+       );
+    });
+}
+$(document).ready(function () {
+    getNewQuote();
+    $('button').on('click', function () {
+        $('#twt').empty();
+        getNewQuote();
+    });
+    
+});
